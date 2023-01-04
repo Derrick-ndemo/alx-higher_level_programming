076aa35c8fe137a9738d885609304ac427d783ae
Derrick-ndemo <derrickndemo@gmail.com>
2023-01-05 02:22:15 +0300

    minor uppercase fix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,5 +2,5 @@
 def uppercase(str):
     for i in str:
         if ord(i) is not range(64, 92):
-            i = chr(ord(i) - 27)
+            i = chr(ord(i) - 32)
         print("{:s}".format(i), end="") 
