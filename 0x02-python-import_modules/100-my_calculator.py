13c42e3d07dc6d47ca4354982f63860598474a0d
Derrick-ndemo <derrickndemo@gmail.com>
2023-01-06 22:58:07 +0300

    personal calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import calculator_1 as calc
+import sys
+
+if __name__ == "__main__":
+    length = len(sys.argv) - 1
+    if length != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    ops = {'+': calc.add, '-': calc.sub, '/': calc.div, '*': calc.mul}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+    
+    
