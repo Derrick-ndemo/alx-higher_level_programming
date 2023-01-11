fd8ac7430f2a863d92b034bb6b299512d8f027bd
Derrick-ndemo <derrickndemo@gmail.com>
2023-01-12 01:01:31 +0300

    roman to intager fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,6 +4,8 @@ def roman_to_int(roman_string):
     rom_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     int_val = 0
 
+    if roman_string != type(str()) or roman_string == None:
+        return 0
     for i in range(len(roman_string)):
         if i > 0 and rom_dict[roman_string[i]] - 2 * rom_dict[roman_string[i - 1]]:
             int_val += rom_dict[roman_string[i]] - 2 * rom_dict[roman_string[i - 1]]
