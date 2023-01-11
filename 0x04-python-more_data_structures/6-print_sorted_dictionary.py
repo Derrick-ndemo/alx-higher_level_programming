53893a6daee74ff776df081cc4e26fec1bce1836
Derrick-ndemo <derrickndemo@gmail.com>
2023-01-11 23:39:24 +0300

    ordered keys fix

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
 def print_sorted_dictionary(a_dictionary):
-    for key, value in a_dictionary.items():
-        print(f"{sorted(key)}: {value}")
+    list_ord = list(a_dictionary.keys())
+    list_ord.sort()
+    for i in list_ord:
+        print("{}: {}".format(i, a_dictionary.get(i)))
