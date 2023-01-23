37e5012a6f410a5b0202063a58a9ee04448bc80e
Derrick-ndemo <derrickndemo@gmail.com>
2023-01-23 14:22:48 +0300

    Intager division with debug

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,9 @@
 
 def safe_print_division(a, b):
     try:
-        res = a / b
-    except (ZeroDivisionError, ValueError, TypeError):)
-        res =  None
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
     finally:
-        print("Inside result: {}".format(res))
-    return res
+        print("Inside result: {}".format(div))
+    return (div)
