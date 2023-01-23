627885867d8fe206c8c3c56edb382d6a9beeea8b
Derrick-ndemo <derrickndemo@gmail.com>
2023-01-23 13:52:09 +0300

    added another exception

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(int(value)))
-    except ValueError:
+    except (ValueError, TypeError):
         return False
     else:
         return True
