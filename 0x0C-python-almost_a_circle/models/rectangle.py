b5b6ef98841e2f656d1d6b920f4f809e29c01f5d
Derrick-ndemo <derrickndemo@gmail.com>
2023-02-13 14:44:09 +0300

    validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -19,6 +19,11 @@ class Rectangle(Base):
             height (int): Height of the Rectangle
             x(int): x value
             y (int): Y value
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
         """
         self.width = width
         self.height = height
@@ -32,7 +37,13 @@ class Rectangle(Base):
 
         @width.setter
         def width(self, value):
+            """Validating the method"""
+            if not isinstance(value, int):
+                raise TypeError('width must be an intager')
+            elif value <= 0:
+                raise ValueError('width must be > 0')
             self.__width = value
+
 
         @property
         def height(self):
@@ -41,6 +52,12 @@ class Rectangle(Base):
 
         @height.setter
         def height(self, value):
+            """Validating the method"""
+            if not isinstance(value, int):
+                raise TypeError('height must be an intager')
+            elif value <= 0:
+                raise ValueError('height must be > 0')
+
             self.__height = value
 
         @property
@@ -50,6 +67,11 @@ class Rectangle(Base):
 
         @x.setter
         def x(self, value):
+            """Validating the method"""
+            if not isinstance(value, int):
+                raise TypeError('x must be an intager')
+            elif value < 0:
+                raise ValueError('x must be >= 0')
             self.__x = value
 
         @property
@@ -59,4 +81,10 @@ class Rectangle(Base):
 
         @y.setter
         def y(self, value):
+            """Validating the method"""
+            if not isinstance(value, int):
+                raise TypeError('y must be an intager')
+            elif value < 0:
+                raise ValueError('y must be >= 0')
+
             self.__y = value
