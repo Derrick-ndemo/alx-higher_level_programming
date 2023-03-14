2a77b031b97a41340dab5cf0d76a88dbd27f1039
Derrick-ndemo <derrickndemo@gmail.com>
2023-03-14 22:42:44 +0300

    List by Best

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- lists all records of the table `second_table` 
+-- results should display both score and name 
+-- results should be ordered by score (top first).
+SELECT score, name FROM second_table ORDER BY SCORE DESC;
