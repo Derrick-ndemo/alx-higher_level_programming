10091454035ea11e355f1b854ca8cd6a4fc257af
Derrick-ndemo <derrickndemo@gmail.com>
2023-03-14 23:08:24 +0300

    Average

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
--- computes the score average of all records
-SELECT AVG(score) AS average
-FROM secodary_table;
+-- Computes the average score of all records in the table second_table.
+SELECT AVG(`score`) AS `average`
+FROM `second_table`;
