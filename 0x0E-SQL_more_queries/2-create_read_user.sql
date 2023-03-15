8925fde981bdb25aace2055450386b2b8f2a001f
Derrick-ndemo <derrickndemo@gmail.com>
2023-03-16 00:20:22 +0300

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,5 @@
 -- Description: Create a read-only user for the database
 CREATE DATABASE IF NOT EXISTS htbn_0d_2;
-USE htbn_0d_2;
-CREATE USER IF NOT EXISTS
+CREATE USER IF NOT EXISTS 
 'usr_0d_2'@'localhost' IDENTIFIED BY 'usr_0d_2_pwd';
 GRANT SELECT ON htbn_0d_2.* TO 'usr_0d_2'@'localhost';
