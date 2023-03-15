18de2e709c97fab5e7e3ecd07be4ac17cec63caa
Derrick-ndemo <derrickndemo@gmail.com>
2023-03-15 22:23:41 +0300

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,4 +3,3 @@
 -- if the uer user_0d_1 already exists, the script should not fail.
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
