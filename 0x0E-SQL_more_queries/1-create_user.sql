aa523658da642e338e537fc4e03687f05e66c4b0
Derrick-ndemo <derrickndemo@gmail.com>
2023-03-15 22:26:24 +0300

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,8 @@
 -- Script that creates the MySQL server user user_0d_1 
 -- The user_0d_1 password should be set to user_0d_1_pwd
 -- if the uer user_0d_1 already exists, the script should not fail.
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER 
+	IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES 
+	ON *.* 
+	TO 'user_0d_1'@'localhost';
