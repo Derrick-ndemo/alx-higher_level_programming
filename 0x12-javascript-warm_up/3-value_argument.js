a8e631c19106659d5bd7d1b5feb6bc2454be8220
Derrick-ndemo <derrickndemo@gmail.com>
2023-04-11 14:58:06 +0300

    argument values

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/*
+ * Script that prints the first argumet passed to it
+ * */
+
+const [arg] = process.argv.slice(2);
+
+if (arg) {
+  console.log(arg);
+} else {
+  console.log('No argument');
+  }
