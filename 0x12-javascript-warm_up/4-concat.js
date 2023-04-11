86262068b9ae34c57a6720126d28d1f8b1cddc58
Derrick-ndemo <derrickndemo@gmail.com>
2023-04-11 19:57:49 +0300

    checker fix

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
-
-console.log(process.argv[2] + 'is' + process.argv[3]);
+const argv1 = process.argv[2];
+const argv2 = process.argv[3];
+console.log(argv1 + 'is' + argv2);
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,10 +2,5 @@
 
 const nums = Number(process.argv[2]);
 const num = Number(process.argv[3]);
-const size = process.argv.length - 2;
-if (isNaN(nums) || size === 1 || isNaN(num)) {
-  console.log('NaN');
-} else {
-  let res = (a, b) => a + b;
-  console.log(res(nums, num));
-}
+const res = (a, b) => a + b;
+console.log(res(nums, num));
