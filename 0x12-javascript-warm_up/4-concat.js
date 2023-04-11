d0d22cfb308ffd4094ad0fe33eadeab57836523b
Derrick-ndemo <derrickndemo@gmail.com>
2023-04-11 20:06:43 +0300

    checker fix

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const argv1 = process.argv[2];
-const argv2 = process.argv[3];
+const { argv } = require('process');
+const argv2 = argv[3]; const argv1 = argv[2];
 console.log(argv1 + 'is' + argv2);
