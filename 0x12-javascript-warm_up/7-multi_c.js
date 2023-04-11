73173fc16de5929e7f0e0f6548cf80c61c1912ec
Derrick-ndemo <derrickndemo@gmail.com>
2023-04-11 17:23:45 +0300

    I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const x = Number(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i <= x - 1; i++) {
+    console.log('C is fun');
+  }
+}
