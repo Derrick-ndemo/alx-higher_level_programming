ca651ae4b34e97eb157fe4270db1d376d9e5db44
Derrick-ndemo <derrickndemo@gmail.com>
2023-04-13 10:55:11 +0300

    fixed indentation

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -3,12 +3,13 @@ module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
-	  this.height = h;
-	}
+      this.height = h;
+    }
   }
+
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
-	}
+    }
   }
 };
