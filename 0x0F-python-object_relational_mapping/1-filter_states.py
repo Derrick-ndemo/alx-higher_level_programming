0bc9e71c064ba8893b07d795e9aa57adff37ce11
Derrick-ndemo <derrickndemo@gmail.com>
2023-06-17 13:20:13 +0300

    Ordered files by asc

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
-        # if row[1][0] == 'N':
-        print(row)
+        if row[1][0] == 'N':
+            print(row)
     cur.close()
     db.close()
